98ba93375dfb6db95160fb52485053ccb710fab8
Benyamin Beyzaie <benyamin.beyzaie@hezardastan.net>
2023-02-23 19:46:31 +0330

    base style

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Button from "@mui/joy/Button";
+import { CssVarsProvider } from "@mui/joy/styles";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <CssVarsProvider>
+      <Button variant="solid">Hello World</Button>
+      <Button>Button</Button>
+      <Button disabled>Disabled</Button>
+      <Button loading>Loading</Button>
+    </CssVarsProvider>
+  </React.StrictMode>
+);
