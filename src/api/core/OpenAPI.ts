f47898657f87e573f251c0c1f0cb4494bff1a212
Benyamin Beyzaie <benyamin.beyzaie@hezardastan.net>
2023-03-04 23:22:18 +0330

    set header in confgi

--- src/api/core/OpenAPI.ts
+++ src/api/core/OpenAPI.ts
@@ -7,25 +7,33 @@ type Resolver<T> = (options: ApiRequestOptions) => Promise<T>;
 type Headers = Record<string, string>;
 
 export type OpenAPIConfig = {
-    BASE: string;
-    VERSION: string;
-    WITH_CREDENTIALS: boolean;
-    CREDENTIALS: 'include' | 'omit' | 'same-origin';
-    TOKEN?: string | Resolver<string>;
-    USERNAME?: string | Resolver<string>;
-    PASSWORD?: string | Resolver<string>;
-    HEADERS?: Headers | Resolver<Headers>;
-    ENCODE_PATH?: (path: string) => string;
+  BASE: string;
+  VERSION: string;
+  WITH_CREDENTIALS: boolean;
+  CREDENTIALS: 'include' | 'omit' | 'same-origin';
+  TOKEN?: string | Resolver<string>;
+  USERNAME?: string | Resolver<string>;
+  PASSWORD?: string | Resolver<string>;
+  HEADERS?: Headers | Resolver<Headers>;
+  ENCODE_PATH?: (path: string) => string;
 };
 
+function getToken() {
+  console.log('getToken', localStorage.getItem('auth'));
+  return localStorage.getItem('auth') == null
+    ? undefined
+    : JSON.parse(localStorage.getItem('auth')!).authToken;
+}
+
 export const OpenAPI: OpenAPIConfig = {
-    BASE: 'http://digivision.pouyaesmaili.ir:8082',
-    VERSION: '1.0',
-    WITH_CREDENTIALS: false,
-    CREDENTIALS: 'include',
-    TOKEN: undefined,
-    USERNAME: undefined,
-    PASSWORD: undefined,
-    HEADERS: undefined,
-    ENCODE_PATH: undefined,
+  BASE: 'http://digivision.pouyaesmaili.ir:8082',
+  VERSION: '1.0',
+  WITH_CREDENTIALS: false,
+  CREDENTIALS: 'include',
+  USERNAME: undefined,
+  PASSWORD: undefined,
+  HEADERS: {
+    Authorization: getToken(),
+  },
+  ENCODE_PATH: undefined,
 };
