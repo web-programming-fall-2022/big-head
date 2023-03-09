a3e98bedb5ecd7fc0a05ff53527097b8dd39d569
fatemeh asgari <fatemeh.asgari@tapsi.cab>
2023-03-09 18:40:44 +0330

    Add favorites page and history page

---
+++ src/modules/favorites/FavoritesPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import WithHeaderLayout from '../../shared/layout/WithHeaderLayout';
+
+function FavoritesPage() {
+  return (
+      <WithHeaderLayout type="title" title="علاقه‌مندی‌ها">
+        <span>علاقه‌مندی‌ها</span>
+      </WithHeaderLayout>
+  )
+}
+
+export default FavoritesPage;
